22c0dc578728d29ca8bf379e71eb511a6c7fa01a
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-03-14 14:13:54 +0100

    Added simple form.

--- app/errors.py
+++ app/errors.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 error = Blueprint('error', __name__)
 
 
 @error.app_errorhandler(404)
 def page_not_found(e):
-    return 'Not Found', 404
+    return render_template('not_found.html'), 404
