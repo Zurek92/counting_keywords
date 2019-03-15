dd67260de3ddc38e648d2f0b13687e9cc906b4fa
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-03-15 21:23:43 +0100

    Added endpoint /keywords which counts keywords.

--- unittests/test_endpoints.py
+++ unittests/test_endpoints.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from unittest.mock import patch
 
 import pytest
 
@@ -21,5 +22,35 @@ def app_fixture():
 )
 def test_simple_endpoint_get(app_fixture, url_path, status_code, template):
     resp = app_fixture.get(url_path)
-    assert resp._status_code == status_code
+    assert resp.status_code == status_code
     assert open_expected_template(current_path, template) in flask_response_data(resp)
+
+
+@pytest.mark.parametrize(
+    'url_path, user_json, mocked_response, expected_output, status_code',
+    (
+        (
+            '/keywords',
+            {'url': 'http://google.com'},
+            '<html><head><meta name="keywords" content="python, javascript" /></head><body>python '
+            '<span>javascript</span><b>python</b></body></html>',
+            {'succes': True, 'words': {'python': 2, 'javascript': 1}},
+            200,
+        ),
+        (
+            '/keywords',
+            {'url': 'http://google.com'},
+            '<html><head></head><body>python ' '<span>javascript</span><b>python</b></body></html>',
+            {'status': 200, 'message': 'This page doesn\'t have any keywords.'},
+            200,
+        ),
+    ),
+)
+@patch('decorators.requests.get')
+def test_simple_endpoint_post(
+    mocked_get, app_fixture, url_path, user_json, mocked_response, expected_output, status_code
+):
+    mocked_get.return_value.text = mocked_response
+    resp = app_fixture.post(url_path, json=user_json)
+    assert resp.status_code == status_code
+    assert resp.get_json() == expected_output
