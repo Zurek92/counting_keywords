b3c467047560398553ee3f948298b1436b0100ec
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-03-15 16:03:54 +0100

    Added get_content decorator.
    Try to get html content or return response bad request.

--- app/decorators.py
+++ app/decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
 
+import requests
 from flask import jsonify, request
 from jsonschema import FormatChecker, validate, ValidationError
 
@@ -8,7 +9,7 @@ from json_validation_schemas import user_json_schema
 
 def check_json(func):
     """Checking json from user."""
-    
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
@@ -19,5 +20,21 @@ def check_json(func):
             response.status_code = 400
             return response
         return func(*args, user_json=user_json, **kwargs)
+
+    return wrapper
+
+
+def get_content(func):
+    """Get html content from url."""
+
+    @wraps(func)
+    def wrapper(user_json, *args, **kwargs):
+        try:
+            html_content = requests.get(user_json['url']).text
+        except Exception:
+            response = jsonify({'status': 400, 'message': 'URL is wrong or page is diabled.'})
+            response.status_code = 400
+            return response
+        return func(*args, user_json={'content': html_content, **user_json}, **kwargs)
 
     return wrapper
--- unittests/test_decorators.py
+++ unittests/test_decorators.py
@@ -1,7 +1,9 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
 from main import app
-from decorators import check_json
+from decorators import check_json, get_content
 
 
 @pytest.mark.parametrize('user_json', ({'url': 'http://www.google.com'}, {'url': 'http://google.com'}))
@@ -26,3 +28,30 @@ def test_check_json_failed(user_json):
             return user_json
 
         assert some_test_func().status_code == 400
+
+
+@patch('decorators.requests.get')
+def test_get_content(mocked_get):
+    mocked_get.return_value.text = '<html><head></head><body></body></html>'
+
+    @get_content
+    def some_test_func(user_json):
+        return user_json
+
+    assert some_test_func({'url': 'http://google.com'}) == {
+        'url': 'http://google.com',
+        'content': '<html><head></head><body></body></html>',
+    }
+
+
+@patch('decorators.requests.get', Mock(side_effect=Exception('wrong url :)')))
+def test_get_content_failed():
+    with app.test_request_context():
+
+        @get_content
+        def some_test_func(user_json):
+            return user_json
+
+        resp = some_test_func({'url': 'www.wrong.url.com'})
+        assert resp.status_code == 400
+        assert resp.get_json() == {'status': 400, 'message': 'URL is wrong or page is diabled.'}
