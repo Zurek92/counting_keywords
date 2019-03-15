d63e042a4fe57fe943e4c8540c6c0db30e8320c3
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-03-16 00:33:41 +0100

    Added JS fetch api with displaying response in table.

--- app/static/scripts/content.js
+++ app/static/scripts/content.js
@@ -1,1 +1,30 @@
+document.querySelector('form button').addEventListener('click', () => {
+    let userUrl = document.querySelector('form input[name="url"]').value
+    let user_json = JSON.stringify({'url': userUrl});
+    fetch('/keywords', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: user_json
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+        let errorMessage = document.querySelector('form div.errorMessage');
+        if (resp['succes']) {
+            errorMessage.innerHTML = ''
+            const tableParts = [];
+            for (let elem in resp['words']) {
+                tableParts.push(`<tr><td>${elem}</td><td>${resp['words'][elem]}</td></tr>`);
+            }
+            let preparedTable = `
+            <tr><th>keyword</th><th>counts</th></tr>
+            ${tableParts.join('')}
+            `
+            document.querySelector('table').innerHTML = preparedTable;
+        } else {
+            errorMessage.innerHTML = resp['message']
+        }
 
+    })
+})
